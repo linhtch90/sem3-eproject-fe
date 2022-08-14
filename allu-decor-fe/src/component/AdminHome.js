cebf97be9529b4bb6d29c01507b55185256afb41
Linh Truong Cong Hong <67230802+linhtch90@users.noreply.github.com>
2022-08-14 22:37:20 +0700

    Fix admin home

--- allu-decor-fe/src/component/AdminHome.js
+++ allu-decor-fe/src/component/AdminHome.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import { Col, Row } from 'antd';
+import { Col, Row, Typography } from 'antd';
 
 import adminHomeImage from '../images/admin/admin-home-page.jpg';
+
+const { Title } = Typography;
 
 const AdminHome = () => {
   return (
     <div>
-      <Row>
+      <Row justify="center" style={{ marginTop: '4rem', marginBottom: '4rem' }}>
         <Col>
-          <img src={adminHomeImage} width={450} height={400} />
+          <Row justify="center">
+            <Title
+              style={{
+                color: '#076678',
+                fontSize: '4rem',
+                fontWeight: 'bolder',
+                textAlign: 'center',
+                textShadow: '6px 6px 0px rgba(131,165,152,0.7)',
+              }}
+            >
+              Welcome to admin page!
+            </Title>
+          </Row>
+          <Row justify="center">
+            <img
+              src={adminHomeImage}
+              width={450}
+              height={400}
+              style={{
+                boxShadow: '10px 10px 0px rgba(131,165,152,0.7)',
+                borderStyle: 'solid',
+                borderWidth: '1px',
+                borderColor: 'rgba(131,165,152,0.7)',
+              }}
+            />
+          </Row>
         </Col>
-        <Col>Welcome to admin page!</Col>
       </Row>
     </div>
   );
